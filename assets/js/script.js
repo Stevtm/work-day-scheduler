cea3e42f7a0bb5cb8d2d061348d488dade782fad
Stevtm <steventmaddison@outlook.com>
2021-03-26 22:55:01 -0400

    update date and time every 1000 ms

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,40 @@
 // initialize luxon variable
 var DateTime = luxon.DateTime;
 
-// get the current date and save in variables
-var date = DateTime.now().toLocaleString(DateTime.DATE_HUGE);
+// ----- functions that show the current date and curren time on the page -----
 
 // render the current date to the DOM
-$("#currentDay").html(`It is ${date}`);
+var getDate = function () {
+	var date = DateTime.now().toLocaleString(DateTime.DATE_HUGE);
+	$("#currentDay").html(`It is ${date}`);
+};
+
+getDate();
+
+// render the curren time to the DOM
+var getTime = function () {
+	var time = DateTime.now().toLocaleString(DateTime.TIME_24_WITH_SECONDS);
+	$("#currentTime").html(time);
+};
+
+getTime();
+
+// ----- format time cells based on relation to current time -----
+// declare variable holding all possible time slots
+var timeSlots = [7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
+
+// add a class to each
+for (var i = timeSlots[0]; i < timeSlots.length + timeSlots[0]; i++) {
+	var test = $(`container['id': '${i}']`);
+	console.log(test);
+}
+
+// ------ update the time and date every second
+// update the current time every second
+setInterval(function () {
+	// update date
+	getDate();
+
+	// update time
+	getTime();
+}, 1000);
